6e0781d18a0a130ce5bed0c21e97945ae9b4cc57
RAMU <excalibur.tiamat@uqmobile.jp>
2018-06-19 23:15:56 +0900

    C-Sharp File

---
+++ Question5.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Algorithm_Practice {
+    public class Question5 {
+        public Question5() {
+        }
+
+        public void Action(int n) {
+            Console.WriteLine("Hanoi {0:d}", n);
+            Hanoi(n, "A", "B", "C");
+        }
+
+        private void Hanoi(int n, String left, String center, String right) {
+            if (n > 0) {
+                Hanoi(n - 1, left, right, center);
+                Console.WriteLine("{0:s} to {1:s}", left, right);
+                Hanoi(n - 1, center, left, right);
+            }
+        }
+    }
+}
