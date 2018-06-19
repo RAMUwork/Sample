42ac9def753a64ab54628830a14572c66e9f46e1
RAMU <excalibur.tiamat@uqmobile.jp>
2018-06-19 23:05:12 +0900

    C\# File

---
+++ Question4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Algorithm_Practice {
+    class Question4 {
+        public Question4() {
+        }
+
+        public void Action(int n, int m) {
+            if (m > n) {
+                int tmp = m;
+                m = n;
+                n = tmp;
+            }
+
+            Console.WriteLine("GCD({0:d},{1:d}) = {2:d}", n, m, CalcGCD(n, m));
+        }
+
+        private int CalcGCD(int n, int m) {
+            if (n == 0) {
+                return m;
+            } else {
+                return CalcGCD(m % n, n);
+            }
+        }
+    }
+}
